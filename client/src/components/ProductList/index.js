c87c53a56a572eaa96c5c80fce6ed89b05f3b249
Craig5117 <craig.bennett5117@gmail.com>
2021-03-17 15:09:59 -0400

    set up indexedDB for products

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -5,27 +5,43 @@ import { UPDATE_PRODUCTS } from '../../utils/actions';
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
+import { idbPromise } from '../../utils/helpers';
 
 function ProductList() {
-const [state, dispatch] = useStoreContext();
-const { currentCategory } = state;
-const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const [state, dispatch] = useStoreContext();
+  const { currentCategory } = state;
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-useEffect(() => {
-  if (data) {
-    dispatch({
-      type: UPDATE_PRODUCTS,
-      products: data.products
-    })
-  }
-}, [data, dispatch]);
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
 
-function filterProducts() {
-  if (!currentCategory) {
-    return state.products;
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
+    }
+  }, [data, loading, dispatch]);
+
+  function filterProducts() {
+    if (!currentCategory) {
+      return state.products;
+    }
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
-  return state.products.filter(product => product.category._id === currentCategory);
-}
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
