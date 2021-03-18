ae95b9cd3a194dac3b6fd6f74b3b6a427f223172
Craig5117 <craig.bennett5117@gmail.com>
2021-03-18 15:49:46 -0400

    removed set cartOpen true from add multiple reducer:

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -39,7 +39,6 @@ export const reducer = (state, action) => {
     case ADD_MULTIPLE_TO_CART:
       return {
         ...state,
-        cartOpen: true,
         cart: [...state.cart, ...action.products],
       };
     case REMOVE_FROM_CART:
