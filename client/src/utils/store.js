c5d285204f6b5ecb7d20aff8a4861f6550109b51
Craig5117 <craig.bennett5117@gmail.com>
2021-03-21 13:54:20 -0400

    commented out preloadedState

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,20 +1,20 @@
 import { createStore } from 'redux';
 import rootReducer from './reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { idbPromise } from './helpers';
+// import { idbPromise } from './helpers';
 
-let preloadedState
-const idbCategories = idbPromise('categoriesList', 'get') || [];
-const idbProducts = idbPromise('productsList', 'get');
-const idbCart = idbPromise('cartList', 'get') || [];
+// let preloadedState
+// const idbCategories = idbPromise('categoriesList', 'get') || [];
+// const idbProducts = idbPromise('productsList', 'get');
+// const idbCart = idbPromise('cartList', 'get') || [];
 
-if (idbProducts && idbCategories) {
-    preloadedState = {
-        productsList: idbProducts,
-        categories: {categoriesList: idbCategories, currentCategory: '' },
-        cart: {cartList: idbCart, cartOpen: false}
-    }
-}
+// if (idbProducts && idbCategories) {
+//     preloadedState = {
+//         productsList: idbProducts,
+//         categories: {categoriesList: idbCategories, currentCategory: '' },
+//         cart: {cartList: idbCart, cartOpen: false}
+//     }
+// }
 
 const composedEnhancer = composeWithDevTools(
     // middleware here if necessary
